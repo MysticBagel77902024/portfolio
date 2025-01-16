b5356da997ab206a8ad87eff3c31d3ff61a2f692
MysticBagel77902024 <mckayerickson@mail.weber.edu>
2025-01-16 12:32:48 -0700

    fixed dark mode

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,19 @@
-function darkMode(){
-	document.body.classList.toggle("darkMode");
-	document.getElementById('darkButton').innerHTML = "Dark Mode Activated";
-}
-function defaultMode(){
-	document.body.classList.toggle("body");
-	document.getElementById('defaultButton').innerHTML="Switch to Light Mode";
-}
+let darkModeText = "Switch to Default Mode";
+let defaultModeText = "Switch to Dark Mode";
+let textContent = "Switch to Dark Mode";
+
+document.addEventListener('DOMContentLoaded', (event) => {
+const button = document.getElementById('darkButton');
+	button.addEventListener("click", function(){
+	//let textContent = "Switch to Dark Mode";
+		if (button.textContent === defaultModeText){
+			button.textContent = darkModeText;
+			document.body.classList.toggle("darkMode");
+		}
+		else {
+			button.textContent = defaultModeText;
+			document.body.classList.toggle("darkMode");
+		}
+	})
+	});
+
